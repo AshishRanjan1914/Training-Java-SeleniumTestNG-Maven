dad0bac883d5e654417c0706f1122940c00cb224
Ashish Ranjan <ashishranjan.biku@gmail.com>
2020-05-24 16:18:18 +0530

    general and constant values

---
+++ src/test/java/general/ConstatntsValues.java
@@ -0,0 +1,7 @@
+package general;
+
+public class ConstatntsValues {
+	
+	public static String myVaribale = "";
+
+}
--- src/test/java/util/DriverUtil.java
+++ src/test/java/util/DriverUtil.java
@@ -1,13 +1,25 @@
 package util;
 
+import java.util.Properties;import org.apache.commons.collections4.map.PassiveExpiringMap.ConstantTimeToLiveExpirationPolicy;
 import org.openqa.selenium.WebDriver;
+
+import general.ConstatntsValues;
 
 public class DriverUtil {
 	
-	private static WebDriver driverUntilDriver ;
+	private WebDriver driverUntilDriver ;
+	private Properties objectProp;
+	private Properties configProp;
+	
 	
 	public DriverUtil(WebDriver driver) {
 		this.driverUntilDriver = driver;		
+	}
+	
+	public DriverUtil(WebDriver driver,Properties objectProp, Properties configProp) {
+		this.driverUntilDriver = driver;	
+		this.configProp = configProp;
+		this.objectProp = objectProp;
 	}
 	
 	public String returnPageTitle() {
@@ -21,11 +33,17 @@ public class DriverUtil {
 	
 	//consider anything..
 	public static boolean isTrue(boolean b) {
+		
+		System.out.println(ConstatntsValues.myVaribale);
 		if(b = true) {
 			return true;
 		} else {
 			return false;
 		}
+	}
+	
+	public void m1() {
+		objectProp.getProperty("MYKEY");
 	}
 
 }
